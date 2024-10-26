9eb61e7d7c7e290521dacdca85610b55e7422539
Omolade Lekan <omoladelekan99@gmail.com>
2024-10-26 18:17:47 +0100

    completed converter features

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ const button = document.querySelector('button');
 const wLoss = document.querySelector('#weight-loss > h3');
 const wLossPercent = document.querySelector('#weight-percent > h3');
 const BMIel = document.querySelector('#BMI > h3');
+const bmiEl = document.querySelector('#BMI');
 
 const form = document.querySelector('.form');
 const result = document.querySelector('.result');
+let health = document.querySelector('.health');
 
 const arrow = document.querySelector('.arrow');
 
@@ -27,11 +29,12 @@ button.addEventListener('click', calcWeigthLoss);
 
 function calcWeigthLoss() {
 
-    const stoneValue = Number(stone.value);
-    const poundsValue = Number(pounds.value);
-    const feetValue = Number(feet.value);
-    const inchesValue = Number(inches.value);
-    const weightChange = Number(pLoss.value);
+    // get input values
+    const stoneValue = Number(stone.value)  || 0;
+    const poundsValue = Number(pounds.value) || 0;
+    const feetValue = Number(feet.value) || 0;
+    const inchesValue = Number(inches.value) || 0;
+    const weightChange = Number(pLoss.value) || 0;
     
 
 
@@ -47,7 +50,8 @@ function calcWeigthLoss() {
 
 
 
-    const weightLoss = Math.round(10 * (weightChange / 100)  * totalWeight) / 10;
+
+    let weightLoss = Math.round(10 * (weightChange / 100)  * totalWeight) / 10;
 
     // converted to kg
     const idealWeight = userWeight - (weightLoss * 6.35);
@@ -55,14 +59,42 @@ function calcWeigthLoss() {
     const BMI = Math.round(idealWeight / userHeight ** 2);
 
 
-    console.log(`${weightChange}%, ${weightLoss}stone, ${BMI}`);
+
+
+    // split weight loss whereever there is point.
+    weightLoss = String(weightLoss).split('.');
 
 
     // set the text content of the element.
-    wLoss.textContent = weightLoss + 'Stone';
+    wLoss.textContent = `${weightLoss[0]}s ${weightLoss[1]}lb`;
     wLossPercent.textContent = weightChange + '%';
     BMIel.textContent = BMI;
 
+
+    // Determine BMI category
+
+    let healthStatus, styleClass; 
+    
+    
+    
+
+    if (BMI < 18.5) {
+        healthStatus = "Underweight";
+        styleClass = "underweight";
+    } else if (BMI < 25) {
+        healthStatus = "Healthy";
+        styleClass = "healthy";
+    } else if (BMI < 30) {
+        healthStatus = "Overweight";
+        styleClass = "overweight";
+    } else {
+        healthStatus = "Obese";
+        styleClass = "obese";
+    }
+
+    // assign health status
+    health.textContent = healthStatus;
+    bmiEl.className = styleClass;
 
 
     form.classList.toggle('hidden');
@@ -91,9 +123,4 @@ arrow.addEventListener('click', () => {
 // to calculate the total pounds(weight) and inches(height) value
 const totalPounds = (stoneValue * 14) + poundsValue;
 const totalInches = (feetValue / 12) * inchesValue;
-
-
-
-
 */
-
